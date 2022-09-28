357b66473f269e9c25168804dda58fa9286ba11f
Navin Pai <lifeofnavin@gmail.com>
2022-09-29 01:58:53 +0530

    Adding behaviour for popup.html to allow creation of new shortlinks

---
+++ src/pages/popup/index.ts
@@ -0,0 +1,32 @@
+import App from "src/components/App.svelte";
+import type { IStorage } from "src/types";
+import {
+  accessToken as accessTokenS,
+  refreshToken as refreshTokenS,
+  domain as domainS,
+  lastVerifiedAt as lastVerifiedAtS,
+} from "../../stores/context";
+
+function loadApp() {
+  chrome.storage.sync.get(
+    {
+      accessToken: "",
+      refreshToken: "",
+      lastVerifiedAt: null,
+      domain: "https://hashbang-api.fly.dev",
+    } as IStorage,
+    ({ accessToken, refreshToken, lastVerifiedAt, domain }: IStorage) => {
+      accessTokenS.set(accessToken);
+      refreshTokenS.set(refreshToken);
+      lastVerifiedAtS.set(lastVerifiedAt);
+      domainS.set(domain);
+
+      const app = new App({
+        target: document.body,
+        props: { mode: "popup" },
+      });
+    }
+  );
+}
+
+document.addEventListener("DOMContentLoaded", loadApp);
