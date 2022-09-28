796898d58622429313e2ff8b6b41d85b21ec2d63
Navin Pai <lifeofnavin@gmail.com>
2022-09-29 02:00:04 +0530

    Components for extension: Popup.svelte and Options.svelte

---
+++ src/components/Popup.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+    import LockOpenSolid from './icons/LockOpenSolid.svelte';
+    import LockSolid from './icons/LockSolid.svelte';
+
+    chrome.runtime.onMessage.addListener((msg) =>{
+        if (msg.type === 'create_link_response') {
+            if(msg.status === 'success') {
+                successMessage = "Success! You can now slash/go to your new shortlink";
+            } else {
+                errorMessage = msg.message;
+            }
+            selectedType = `static`;
+        }
+    });
+    
+    let successMessage: string = null, errorMessage: string = null;
+    let shortLink: string = '', isPrivate: boolean = false;
+    let selectedType: string = `static`;
+    let dynamicUrl: string = '';
+    
+    //TODO: Make this Dynamic
+    const slashGoHero = `go/`;
+    const privatePath = `my/`;
+    $: displayPath = isPrivate ? slashGoHero + privatePath : slashGoHero;
+
+    const createLink = async (url: string) => {
+        errorMessage = ''; 
+        successMessage = '';
+        
+        chrome.runtime.sendMessage({command: 'create_link', data: {url, shortLink, selectedType, isPrivate }})    
+    }
+    
+    const setDynamicUrl = () => {
+        chrome.tabs.query({ active:true },
+        tabs => { 
+            console.info(`creating shortlink for ${tabs[0].url}`)
+            dynamicUrl = tabs[0].url 
+        });
+    }
+    
+    $: submit = async () => {
+        chrome.tabs.query({ active:true },
+        tabs => { 
+            console.info(`creating shortlink for ${tabs[0].url}`)
+            createLink(selectedType === `dynamic`? dynamicUrl : tabs[0].url); 
+        });
+    }
+</script>
+
+<section class="antialiased bg-gray-100 text-gray-600 h-screen px-4">
+    <div class="flex flex-col justify-center h-full">
+<div class="main bg-slate-100">
+    <div class="p-2">
+        <form on:submit|preventDefault={submit}>
+            <div class="flex">
+                <div class="ease-in select-none font-bold text-slate-500 ml-1 mt-1 mb-1 p-2 text-lg border border-slate-300 rounded-l-md text-left">{displayPath}</div>
+                <input bind:value={shortLink} type="input" class="text-lg text-slate-500 mt-1 mb-1 pl-1 border border-slate-300 text-left" placeholder="shortlink">
+                <div on:click={() => isPrivate = !isPrivate} class="ease-in hover:cursor-pointer select-none text-slate-500 {isPrivate ? 'bg-gray-300': '' } text-sm mt-1 p-2 mb-1 text-lg border border-slate-300 rounded-r-md text-left">
+                    {#if isPrivate} 
+                        <LockSolid size='20px'/>
+                    {:else}
+                        <LockOpenSolid size='20px'/>
+                    {/if}
+                </div>
+            </div>
+            
+            <div class="flex justify-center">
+                <div class="ease-in p-2 rounded select-none {selectedType == `static` ? 'bg-gray-300 text-gray-600 underline underline-offset-4': 'text-gray-400' } hover:text-gray-600 hover:cursor-pointer text-left font-bold" on:click={_ => selectedType = `static`}>static</div>
+                <div class="ease-in p-2 rounded select-none {selectedType == `dynamic` ? 'bg-gray-300 text-gray-600 underline underline-offset-4': 'text-gray-400' } hover:text-gray-600 hover:cursor-pointer text-left font-bold" on:click={_ => { selectedType = `dynamic`; setDynamicUrl();}}>dynamic</div>
+            </div>
+            {#if selectedType == `dynamic`}
+                <div class="mt-2 text-center">
+                    <div class="text-xs text-green-400">Replace dynamic part of url by <span class="text-bold font-bold">&lt;var&gt;</span></div>
+                    <input bind:value={dynamicUrl} type="input" class="w-full text-lg text-slate-600 rounded mt-1 mb-1 pl-2 border border-slate-300 text-left" placeholder="shortlink">
+                </div>    
+            {/if}
+            <div class="w-100 text-center">
+                <button disabled='{shortLink.length == 0}' class="disabled:opacity-50 mt-2 w-60 mb-2 bg-red-500 hover:bg-red-600 text-white font-bold pt-2 pb-2 pl-6 pr-4 rounded" type="submit">Create</button>
+            </div>
+        </form>
+        {#if successMessage}<div class="p-2 text-center text-green-500 font-bold">{successMessage}</div>{/if}
+        {#if errorMessage}<div class="p-2 text-center text-red-500 font-bold">{errorMessage}</div>{/if}
+    </div>
+</div>
+</div>
+</section>
---
+++ src/pages/options/index.ts
@@ -0,0 +1,30 @@
+import App from "src/components/App.svelte";
+import type { IStorage } from "src/types";
+import {
+  accessToken as accessTokenS,
+  refreshToken as refreshTokenS,
+  lastVerifiedAt as lastVerifiedAtS,
+} from "../../stores/context";
+
+function loadApp() {
+  chrome.storage.sync.get(
+    {
+      accessToken: "",
+      refreshToken: "",
+      lastVerifiedAt: null,
+      domain: "https://hashbang-api.fly.dev",
+    } as IStorage,
+    ({ accessToken, refreshToken, lastVerifiedAt, domain }: IStorage) => {
+      accessTokenS.set(accessToken);
+      refreshTokenS.set(refreshToken);
+      lastVerifiedAtS.set(lastVerifiedAt);
+
+      const app = new App({
+        target: document.body,
+        props: { mode: "options" },
+      });
+    }
+  );
+}
+
+document.addEventListener("DOMContentLoaded", loadApp);
