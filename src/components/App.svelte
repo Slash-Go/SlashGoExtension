56db5ff66509ce9543741e9e24549764ab14f9bf
Navin Pai <lifeofnavin@gmail.com>
2022-09-29 01:57:36 +0530

    Base svelte class for Options and Popup

---
+++ src/components/App.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+	<link rel="stylesheet" href="../../../css/index.css">
+</svelte:head>
+
+<script lang="ts">
+    import { accessToken} from '../stores/context'
+    import Login from './Login.svelte';
+    import Options from './Options.svelte';
+    import Popup from './Popup.svelte';
+
+    export let mode = `popup`;  
+    
+    chrome.runtime.onMessage.addListener((msg) =>{
+        if (msg.type === 'logout') {
+            $accessToken = '';
+        };
+    });
+
+</script>
+
+<div class="w-full">
+    {#if $accessToken.length != 0}
+        {#if mode === `options`}
+            <Options />
+        {:else}
+            <Popup />
+        {/if}
+    {:else}
+        <Login />
+    {/if}
+</div>
