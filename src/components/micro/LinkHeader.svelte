83da8fec9ab5bfc8f092e695e2e25661f6d26931
Sanyam Jain <xerosanyam@gmail.com>
2022-10-06 21:48:49 +0530

    feat(link-header): extract out links as separate component

---
+++ src/components/micro/LinkHeader.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let value;
+  export let label;
+
+  export let choice;
+  export let updateChoice;
+</script>
+
+<div
+  class={`${
+    choice === value
+      ? "text-white bg-red-500  cursor-default font-semibold"
+      : "hover:text-red-600 hover:underline hover:underline-offset-4 hover:cursor-pointer pr-4"
+  } px-4 py-2 `}
+  on:click={() => updateChoice(value)}
+>
+  {label}
+</div>
