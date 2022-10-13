05a19e369423ae135c4634f4dd0e73b27f935e92
Navin Pai <lifeofnavin@gmail.com>
2022-10-13 17:13:34 +0530

    [MINOR] Prettier formatting fixes

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -57,13 +57,13 @@
   });
 </script>
 
-  {#if successMessage}<div class="p-2 text-center text-green-500 font-bold">
-      {successMessage}
-    </div>{/if}
-  {#if errorMessage}<div class="p-2 text-center text-red-500 font-bold">
-      {errorMessage}
-    </div>{/if}
-    <div class="overflow-x-auto p-3">
+{#if successMessage}<div class="p-2 text-center text-green-500 font-bold">
+    {successMessage}
+  </div>{/if}
+{#if errorMessage}<div class="p-2 text-center text-red-500 font-bold">
+    {errorMessage}
+  </div>{/if}
+<div class="overflow-x-auto p-3">
   {#if isLoading}
     <Loader />
   {:else}
