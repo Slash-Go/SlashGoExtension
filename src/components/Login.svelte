7ab6b899f1e92d596b700fa7790e57172e93f1ea
Navin Pai <lifeofnavin@gmail.com>
2022-10-19 02:34:40 +0530

    Forgot password workflow for extension

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -12,10 +12,13 @@
 
   let email: string = "",
     password: string = "",
-    errorMessage = "",
-    moreOptions = false;
+    errorMessage: string = "",
+    successMessage: string = "",
+    moreOptions: boolean = false,
+    resetPasswordMode: boolean = false;
 
-  let isLoggingIn: boolean = false;
+  let isLoggingIn: boolean = false,
+    isResettingPassword: boolean = false;
 
   const save = async () => {
     const storage: IStorage = {
@@ -38,87 +41,155 @@
   };
 
   $: submit = async () => {
-    isLoggingIn = true;
-    axios
-      .post(`${$domain}/auth/login`, {
-        email,
-        password,
-      })
-      .then((response: AxiosResponse) => {
-        if (response.status === 200) {
-          $accessToken = response.data.accessToken;
-          $refreshToken = response.data.refreshToken;
-          $role = response.data.role;
-          $orgHero = response.data.orgHero;
-          $lastVerifiedAt = new Date().toISOString();
-          errorMessage = "";
-          save();
-        }
-      })
-      .catch((e: AxiosError) => {
-        console.log("Could not log in user");
-        if (e.response!.status == 401) {
-          errorMessage = e.response.data["error"] as string;
-        } else {
-          errorMessage = `Unable to log user in`;
-        }
-      })
-      .finally(() => {
-        isLoggingIn = false;
-      });
+    if (!resetPasswordMode) {
+      isLoggingIn = true;
+      axios
+        .post(`${$domain}/auth/login`, {
+          email,
+          password,
+        })
+        .then((response: AxiosResponse) => {
+          if (response.status === 200) {
+            $accessToken = response.data.accessToken;
+            $refreshToken = response.data.refreshToken;
+            $role = response.data.role;
+            $orgHero = response.data.orgHero;
+            $lastVerifiedAt = new Date().toISOString();
+            errorMessage = "";
+            save();
+          }
+        })
+        .catch((e: AxiosError) => {
+          console.log("Could not log in user");
+          if (e.response!.status == 401) {
+            errorMessage = e.response.data["error"] as string;
+          } else {
+            errorMessage = `Unable to log user in`;
+          }
+        })
+        .finally(() => {
+          isLoggingIn = false;
+        });
+    } else {
+      isResettingPassword = true;
+      axios
+        .post(`${$domain}/auth/reset_password_request`, {
+          email,
+        })
+        .then((response: AxiosResponse) => {
+          if (response.status === 200) {
+            successMessage = response.data.message;
+          }
+        })
+        .catch((e: AxiosError) => {
+          console.log("Could not reset password for user");
+          if (e.response!.status == 400) {
+            errorMessage = e.response.data["error"] as string;
+          } else {
+            errorMessage = `Unable to reset password for user`;
+          }
+        })
+        .finally(() => {
+          isResettingPassword = false;
+        });
+    }
   };
 </script>
 
-<div class="h-screen bg-slate-100">
-  <div
-    class="flex justify-center text-center text-xl bg-slate-200 p-1 border-b-2 border-slate-400"
-  >
-    <span class="text-black-500 font-bold">Slash</span>
-    <span class="text-red-500 font-bold">/Go</span>
-  </div>
+<div class="flex md:h-screen">
+  <div class="m-auto bg-slate-100">
+    <div
+      class="flex justify-center text-center text-xl bg-slate-200 p-1 border-b-2 border-slate-400"
+    >
+      <img alt="header" width="60px" src="../../../images/slashgo_header.png" />
+    </div>
 
-  {#if errorMessage}
-    <div class="p-2 text-center text-red-600 font-bold">{errorMessage}</div>
-  {/if}
-
-  <div class="p-2">
-    <form class="text-center" on:submit|preventDefault={submit}>
-      <input
-        bind:value={email}
-        type="email"
-        class="m-1 p-2 border border-slate-300 rounded-md text-center"
-        placeholder="username@domain.com"
-        required
-      />
-      <input
-        bind:value={password}
-        type="password"
-        class="m-1 p-2 border border-slate-300 rounded-md text-center"
-        placeholder="password"
-        required
-      />
-
-      <div
-        class="select-none pt-2 pl-2 text-xs hover:text-gray-600 hover:cursor-pointer text-left font-bold text-gray-400"
-        on:click={(_) => (moreOptions = !moreOptions)}
-      >
-        (+)
+    {#if errorMessage}
+      <div class="p-2 text-center text-red-600 font-bold">{errorMessage}</div>
+    {/if}
+    {#if successMessage}
+      <div class="p-2 text-center text-green-600 font-bold">
+        {successMessage}
       </div>
-      {#if moreOptions}
+    {/if}
+
+    <div class="p-2">
+      <form class="text-center" on:submit|preventDefault={submit}>
         <input
-          bind:value={$domain}
-          type="string"
+          bind:value={email}
+          on:focus={() => {
+            errorMessage = "";
+            successMessage = "";
+          }}
+          type="email"
           class="m-1 p-2 border border-slate-300 rounded-md text-center"
-          placeholder="slashGo domain"
+          placeholder="username@domain.com"
           required
         />
-      {/if}
+        {#if !resetPasswordMode}
+          <input
+            bind:value={password}
+            on:focus={() => {
+              errorMessage = "";
+              successMessage = "";
+            }}
+            type="password"
+            class="m-1 p-2 border border-slate-300 rounded-md text-center"
+            placeholder="password"
+            required
+          />
+        {/if}
 
-      <button
-        disabled={isLoggingIn}
-        class="disabled:opacity-50 mt-2 mb-2 bg-red-500 hover:bg-red-600 text-white font-bold p-2 rounded"
-        type="submit">{isLoggingIn ? "Signing in..." : "Sign In"}</button
-      >
-    </form>
+        <div
+          class="select-none pt-2 pl-2 text-xs hover:text-gray-600 hover:cursor-pointer text-left font-bold text-gray-400"
+          on:click={(_) => (moreOptions = !moreOptions)}
+        >
+          (+)
+        </div>
+        {#if moreOptions}
+          <input
+            bind:value={$domain}
+            type="string"
+            class="m-1 p-2 border border-slate-300 rounded-md text-center"
+            placeholder="SlashGo domain"
+            required
+          />
+        {/if}
+
+        {#if !resetPasswordMode}
+          <button
+            disabled={isLoggingIn}
+            class="disabled:opacity-50 mt-2 mb-2 bg-red-500 hover:bg-red-600 text-white font-bold p-2 rounded select-none hover:cursor-pointer"
+            type="submit">{isLoggingIn ? "Signing in..." : "Sign In"}</button
+          >
+
+          <div
+            on:click={() => {
+              resetPasswordMode = true;
+            }}
+            class="underline text-center hover:cursor-pointer"
+          >
+            Forgot Password?
+          </div>
+        {:else}
+          <button
+            disabled={isResettingPassword}
+            class="disabled:opacity-50 mt-2 mb-2 bg-red-500 hover:bg-red-600 text-white font-bold p-2 rounded hover:cursor-pointer select-none"
+            type="submit"
+            >{isResettingPassword
+              ? "Requesting ..."
+              : "Request Password Reset"}</button
+          >
+          <div
+            on:click={() => {
+              resetPasswordMode = false;
+            }}
+            class="underline text-center hover:cursor-pointer"
+          >
+            Go Back?
+          </div>
+        {/if}
+      </form>
+    </div>
   </div>
 </div>
